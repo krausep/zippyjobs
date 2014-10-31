081f4fe6558a41fe28994ad33134fdb56d9a0952
Paul Krause <krausep@gmail.com>
2014-10-31 06:37:51 -0500

    adding in new controllers

---
+++ ZippyJobs/Controllers/JobController.cs
@@ -0,0 +1,40 @@
+﻿using Couchbase;
+using Couchbase.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ZippyJobs.Models;
+
+namespace ZippyJobs.Controllers
+{
+    public class JobController : ApiController
+    {
+        private static readonly CouchbaseClient Client = new CouchbaseClient();
+
+        [HttpGet]
+        [Route("api/job")]
+        public IHttpActionResult Job()
+        {
+            var view = Client.GetView("child", "jobs");
+            var results = view.Select(job => Client.GetJson<Job>(job.ItemId)).ToList();
+
+            return Ok(results);
+        }
+
+        [HttpGet]
+        [Route("api/job/{jobId}")]
+        public IHttpActionResult Job(int jobId)
+        {
+            var c = new Job { JobId = jobId };
+            var job = Client.GetJson<Job>(c.Key);
+
+            if (job == null)
+                return NotFound();
+
+            return Ok(job);
+        }
+    }
+}
---
+++ ZippyJobs/Controllers/RewardController.cs
@@ -0,0 +1,40 @@
+﻿using Couchbase;
+using Couchbase.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ZippyJobs.Models;
+
+namespace ZippyJobs.Controllers
+{
+    public class RewardController : ApiController
+    {
+        private static readonly CouchbaseClient Client = new CouchbaseClient();
+
+        [HttpGet]
+        [Route("api/reward")]
+        public IHttpActionResult Reward()
+        {
+            var view = Client.GetView("child", "rewards");
+            var results = view.Select(reward => Client.GetJson<Reward>(reward.ItemId)).ToList();
+
+            return Ok(results);
+        }
+
+        [HttpGet]
+        [Route("api/reward/{rewardId}")]
+        public IHttpActionResult Reward(int rewardId)
+        {
+            var c = new Reward { RewardId = rewardId };
+            var reward = Client.GetJson<Reward>(c.Key);
+
+            if (reward == null)
+                return NotFound();
+
+            return Ok(reward);
+        }
+    }
+}
